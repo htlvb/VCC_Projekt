97b5436e0f879c55e3b9d7e95c95e60a9e29a94b
e.lassl <emma.lassl193@gmail.com>
2025-02-06 15:24:41 +0100

    lase_EventDescriptionAndDate

--- VCC_Projekt/GlobalUsing.cs
+++ VCC_Projekt/GlobalUsing.cs
@@ -1,8 +1,6 @@
-﻿global using Microsoft.AspNetCore.Authorization;
-global using Microsoft.EntityFrameworkCore;
-global using Microsoft.AspNetCore.Components.Authorization;
+﻿global using Microsoft.AspNetCore.Components.Authorization;
 global using Microsoft.AspNetCore.Identity;
+global using Microsoft.EntityFrameworkCore;
 global using VCC_Projekt.Components;
 global using VCC_Projekt.Components.Account;
 global using VCC_Projekt.Data;
-global using Pomelo.EntityFrameworkCore;
