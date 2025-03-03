ca0e9d20890ca0a73aaa2e02014283ea10308654
Marco Maie <Marcomaier1006@outlook.com>
2025-03-03 08:56:43 +0100

    formate document

--- VCC_Projekt/Controllers/FileController.cs
+++ VCC_Projekt/Controllers/FileController.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Cors;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using System.Text;
 
 namespace VCC_Projekt.Controllers
 {
