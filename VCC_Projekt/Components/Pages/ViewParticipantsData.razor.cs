f0bd74312db3cc703078460cf2fba2dd6bf24c2f
w.smolinsky <w.smolinsky@htlvb.at>
2025-03-06 14:53:03 +0100

    razor, cs and ddl created

---
+++ VCC_Projekt/Components/Pages/ViewParticipantsData.razor.cs
@@ -0,0 +1,22 @@
+﻿using VCC_Projekt.Data;
+
+namespace VCC_Projekt.Components.Pages
+{
+    public partial class  ViewParticipantsData
+    {
+        private Event _selectedEvent = new() { EventID = 0 };
+        private List<Event> _events = new();
+        protected override void OnInitialized()
+        {
+            _events = dbContext.Events.OrderByDescending(ev => ev.Beginn).ToList();
+        }
+
+        private async Task OnEventSelected(Event selectedEvent)
+        {
+            _selectedEvent = selectedEvent;
+            //_levels = _selectedEvent.EventID != 0
+            //    ? await LoadLevelsAsync()
+            //    : new List<LevelViewModel>();
+        }
+    }
+}
