86220dc080f2629fa23ca6e619ac23e01734c6bd
m.maier <m.maier@htlvb.at>
2025-03-19 07:54:51 +0100

    formate

--- VCC_Projekt/Components/Account/EmailSender.cs
+++ VCC_Projekt/Components/Account/EmailSender.cs
@@ -3,7 +3,6 @@ using MailKit.Net.Imap;
 using MailKit.Search;
 using Microsoft.Extensions.Options;
 using MimeKit;
-using System.Globalization;
 using System.Net;
 using System.Net.Mail;
 using System.Text;
