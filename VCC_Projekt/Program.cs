958abeb48e215b735ec8d396a51d507b28a95e2e
Marco <Marcomaier1006@outlook.com>
2025-04-09 18:07:55 +0200

    implimented docker secrets

--- VCC_Projekt/Program.cs
+++ VCC_Projekt/Program.cs
@@ -66,8 +66,8 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    builder.Configuration.AddKeyPerFile("/run/secrets", optional: true);
 }
 
 app.UseHttpsRedirection();
